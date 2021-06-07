6e10d759798791a833a909ef81b73161874b7484
robin dauteuil <robin.dauteuil@live.fr>
2021-06-07 15:49:20 +0200

    script initialisation BDD

---
+++ main.py
@@ -0,0 +1,34 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def connect():
+    """Connect to Mysql database"""
+
+
+connexion = mysql.connector.connect(
+    host='localhost', user='robin', password=''
+)
+
+cursor = connexion.cursor()
+
+cursor.execute(
+    'create database if not exists open_food_facts'
+)
+cursor.execute(
+    'use open_food_facts'
+)
+cursor.execute(
+    'create table if not exists category('
+    'ID smallint unsigned not null auto_increment primary key, name varchar(150) not null '
+    ') engine = innodb;'
+)
+cursor.execute('create table if not exists food_items(ID smallint unsigned not null auto_increment primary key,'
+               ' name varchar(150) not null, category varchar(150) not null, substitute_food varchar (255), description varchar(255), shop varchar(50)) engine = innodb')
+
+cursor.execute(
+    'create table if not exists registred_food(ID smallint unsigned not null auto_increment primary key, name varchar(150)) engine = innodb')
+cursor.execute('show tables')
+
+for x in cursor:
+    print(x)
