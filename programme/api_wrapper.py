593fbd0e7f658e013af894e969cc4bad54535126
robin dauteuil <robin.dauteuil@live.fr>
2021-09-13 15:50:53 +0200

    sélection produitavec menu

--- programme/api_wrapper.py
+++ programme/api_wrapper.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import binance
+
 from programme.bdd_wrapper import *
 
 
@@ -8,87 +8,93 @@ class Api:
 
     def __init__(self):
 
-        self.url_list = None
-        self.list_code_products_from_a_category = None
-
+        self.category_id = None
+        self.id_dict = None
+        self.list_categories = None
     def get_categories(self):
-        url_list = {}
+        name_dict = {}
+        id_list = []
+        list_categories = []
         url_requests_category = "https://france.openfoodfacts.org/categories.json"
         all_categories = requests.get(url_requests_category).json()
         tags_categories = all_categories['tags']
         # print(tags_categories)
         # print(list(tags_categories.keys()))
+        n = 1
         for categories in tags_categories:
-            if len(url_list) < 10:
-                url_list[categories['name']] = categories['url']
 
-        self.url_list = url_list
+            if len(name_dict) < 30:
+                name_dict[n] = categories['name']
+                id_list.append(categories['id'])
+                list_categories.append(categories['name'])
+                n += 1
 
-        print(self.url_list)
-        return self.url_list
-    def get_products_from_a_category(self, category_name):
 
-        list_code_products_from_a_category = []
-        list_url_products = []
-        url = self.url_list[category_name]
-        response_category_request = requests.get(url + '.json').json()
-        products_from_1_category = response_category_request['products']
-        for products in products_from_1_category:
-            if products.get('code') is not None:
-                list_code_products_from_a_category.append(products.get('code'))
-            if products.get('url') is not None:
-                list_url_products.append(products.get('url'))
+        return name_dict, id_list, list_categories
 
-        self.list_code_products_from_a_category = list_code_products_from_a_category
-        # print(self.list_code_products_from_a_category)
-        # print(list_url_products)
-        #
-        #    #products = api.get_products_from_one_category("bio")
-        # #def get_infos_from_product(self):
+    def get_products_from_a_category(self, category_id):
 
+        url = "https://us.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=" + category_id + "&tagtype_1=nutrition_grades&tag_contains_1=contains&tag_1=A&additives=without&ingredients_from_palm_oil=without&json=true"
+        response_category_request = requests.get(url).json()
+
+        produit = response_category_request['products']
+
+        products_name_list = []
         categories_products_list = {}
         nutriscore_list = {}
         stores_list = {}
         brands_list = {}
-        products_name_list = []
-        for code in self.list_code_products_from_a_category:
-            reponse = requests.get(
-                "https://fr.openfoodfacts.org/api/v0/produit/" + code + ".json").json()
-            produit = reponse['product']
+        list_url = {}
+        description_list = {}
 
 
-            nom_produit = (produit["product_name"])
+        for products in produit:
+            nom_produit = (products["product_name"])
             products_name_list.append(nom_produit)
 
-            categories_produit = (produit["categories"])
-            #categories_products_list.append(categories_produit)
+            url_produit = (products['url'])
+            list_url[nom_produit] = url_produit
+
+            categories_produit = (products["categories"])
             categories_products_list[nom_produit] = categories_produit
 
-            #
             try:
-                nutriscore = produit['nutrition_grades']
+                nutriscore = products['nutrition_grades']
             except:
-                nutriscores = 'inconnu'
-            #nutriscore_list.append(nutriscore)
+                nutriscore = 'inconnu'
             nutriscore_list[nom_produit] = nutriscore
 
-
             try:
-                marque = produit['brands']
+                marque = products['brands']
             except:
                 marque = 'marque inconnu'
-            #brands_list.append(marque)
             brands_list[nom_produit] = marque
 
             try:
-                magasin_dispo = produit['stores']
+                magasin_dispo = products['stores']
             except:
                 magasin_dispo = 'magasin inconnu'
-            #stores_list.append(magasin_dispo)
             stores_list[nom_produit] = magasin_dispo
 
-        return products_name_list, categories_products_list, nutriscore_list, stores_list, brands_list
 
+
+            try:
+                description = products['ecoscore_data']['agribalyse']['name_fr']
+            except:
+                description = 'description inconnu'
+            description_list[nom_produit] = description
+
+        return products_name_list, categories_products_list, nutriscore_list, stores_list, brands_list, list_url, description_list
+
+    def get_category_id(self, id_list, category_id):
+
+        for nb, val in enumerate(id_list, start=1):
+            if val == category_id:
+                nb_category = nb
+                # list_keys = list(id_list.keys())
+                # nb_category = list_keys.index(k)
+
+                return nb_category
 
 # 1) finir fonction get_product_1_category => liste code et url des produits pour la catégorie en parametre
 
@@ -96,8 +102,13 @@ class Api:
 # 3)récuperer name, category, nutriscore, magasin, marque de chaque produit pour la catégorie sélectionnée depuis la requete
 # 4)organisation en module
 # 5)insertion données dans les tables
-#)affichage les catégories, les produits
+# )affichage les catégories, les produits
 # 6)sélection d'un produit et comparaison produits pour trouver un substitut
-#)enregister un substitut
+# )enregister un substitut
 # 7)L'utilisateeur peut accéder à sa  base de données
 
+#etat: initial
+#bonjour veuillez selecttionner une option
+#1 creer une relation substituant substitué
+#2voir les relations deja creer
+#3quitter
--- programme/bdd_wrapper.py
+++ programme/bdd_wrapper.py
@@ -9,13 +9,12 @@ class Data_base:
         self.connexion = mysql.connector.connect(
             host='localhost', user='robin', password=''
         )
-        self.cursor = self.connexion.cursor()
-
+        self.cursor = self.connexion.cursor(buffered=True)
+        self.categories_loaded = False
+        self.list_category = None
 
     def initialisation(self):
         """Connect and create Mysql database"""
-
-
 
         self.cursor.execute(
             'create database if not exists open_food_facts'
@@ -29,22 +28,20 @@ class Data_base:
             ') engine = innodb;'
         )
         self.cursor.execute('create table if not exists food_items(ID smallint unsigned not null auto_increment ,'
-                       ' name varchar(150) , category_ID smallint unsigned , categories_tags text , description varchar(255),nutriscore varchar(1), shop varchar(50), brand varchar(50), primary key (ID)) engine = innodb')
+                            ' name varchar(150) , category_ID smallint unsigned , description varchar(255),nutriscore varchar(1), shop varchar(255), brand varchar(255), url text, primary key (ID)) engine = innodb')
 
         self.cursor.execute(
-            'create table if not exists registred_food(ID smallint unsigned not null auto_increment primary key, name varchar(150)) engine = innodb')
+            'create table if not exists registred_food(ID smallint unsigned not null auto_increment , name varchar(150), primary key (ID)) engine = innodb')
 
-    def insert_products(self, list):
+    def insert_products(self, list, nb):
         self.cursor.execute(
             'use open_food_facts'
         )
-        products_name_list, categories_products_list, nutriscore_list, stores_list, brands_list = list
-        print(nutriscore_list)
-        print(products_name_list)
+        products_name_list, categories_products_list, nutriscore_list, stores_list, brands_list, list_url, description_list = list
 
         for nom_produit in products_name_list:
             requete = "insert into food_items (name) values (%s);"
-            value = [(nom_produit )]
+            value = [(nom_produit)]
             self.cursor.execute(requete, value)
 
         for keys, values in nutriscore_list.items():
@@ -52,22 +49,111 @@ class Data_base:
             value = [(values, keys)]
             self.cursor.executemany(requete, value)
 
-        for keys, values in categories_products_list.items():
-             requete = "update food_items set categories_tags = (%s) where name = (%s )"
-             value = [(values, keys)]
-             self.cursor.executemany(requete, value)
-
+        for keys, values in description_list.items():
+            requete = "update food_items set description = (%s) where name = (%s )"
+            value = [(values, keys)]
+            self.cursor.executemany(requete, value)
 
         for keys, values in stores_list.items():
-             requete = "update food_items set shop = (%s) where name = (%s )"
-             value = [(values, keys)]
-             self.cursor.executemany(requete, value)
+            requete = "update food_items set shop = (%s) where name = (%s )"
+            value = [(values, keys)]
+            self.cursor.executemany(requete, value)
         #
         for keys, values in brands_list.items():
             requete = "update food_items set brand = (%s) where name = (%s )"
-            value = [(values , keys)]
+            value = [(values, keys)]
             self.cursor.executemany(requete, value)
-        #
-        #         # resultat = self.cursor.fetchall()
-        #         # print("resultat = ", resultat)
+
+        for keys, values in list_url.items():
+            requete = "update food_items set url = (%s) where name = (%s )"
+            value = [(values, keys)]
+            self.cursor.executemany(requete, value)
+
+        for nom_produit in products_name_list:
+            requete = 'update food_items set category_ID = (%s) where name = (%s)'
+            value = [(int(nb), nom_produit)]
+            self.cursor.executemany(requete, value)
         self.connexion.commit()
+        #
+
+    def checking_init(self):
+
+        requete = "select exists(select 1 from category);"
+        self.cursor.execute(requete)
+        result_cat = self.cursor.fetchall()
+
+        # result = self.cursor.fetchall()
+        if result_cat == [(1,)]:
+            self.categories_loaded = True
+            print('pre_loaded')
+
+    def insert_categories(self, list_categories):
+
+        for category in list_categories:
+            requete = "insert into category(name) values(%s);"
+            value = [(category)]
+            self.cursor.execute(requete, value)
+        self.connexion.commit()
+        self.categories_loaded = True
+
+    def select_a_category(self, offset):
+
+        # self.cursor.execute(requete)
+        self.cursor.execute('select * from category as categorie limit %s offset %s', (20, int(offset)))
+        resultat = self.cursor.fetchall()
+        for tuple in resultat:
+            print('{} . {}'.format(resultat.index(tuple) + 1, tuple[1]))
+
+        self.list_category = resultat
+
+    def show_all_products_from_a_cat(self, category_id, offset):
+
+        for tuple in self.list_category:
+            nb, name = self.list_category.index(tuple)+1, tuple[1]
+            if nb == category_id:
+                # name = name_category
+
+                requete = 'select ID from category where name = %s '
+                self.cursor.execute(requete, (name,))
+                res = self.cursor.fetchall()
+
+                nb = str(res).strip('[]')
+                id = nb.strip('()')
+                re_id = id.strip(',')
+                int_id = int(re_id)
+                requete = ('select * from food_items where category_id = (%s) limit 20 offset %s', (int_id, offset))
+                print('category :', name)
+                self.cursor.execute(*requete)
+        resultat = self.cursor.fetchall()
+
+        for tuple in resultat:
+            print('{} . {}'.format(resultat.index(tuple) + 1, tuple[1]))
+        return resultat, int_id
+
+    def select_a_product(self, id_product, products):
+        for tuple in products:
+            nb = products.index(tuple)+1
+            name = tuple[1]
+
+            if nb == id_product:
+                requete = 'select * from food_items where name = %s '
+                self.cursor.execute(requete, (name,))
+                res = self.cursor.fetchall()
+                print(res)
+                return nb-1
+
+        self.connexion.commit()
+
+    def show_substituants(self, nb, offset):
+
+        requete = ('select * from food_items where category_id = (%s) limit 20 offset %s', (nb, offset))
+
+        self.cursor.execute(*requete)
+
+        resultat = self.cursor.fetchall()
+        resultat.pop(nb)
+        for tuple in resultat:
+            print('{} . {}'.format(resultat.index(tuple) + 1, tuple[1]))
+
+
+
